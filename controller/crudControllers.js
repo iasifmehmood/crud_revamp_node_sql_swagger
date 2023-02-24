5023c7081a41691ded68dc71286662d779242178
asif-mehmood@hotmail.com <asif-mehmood@hotmail.com>
2023-02-24 12:27:59 +0500

    added email and password validation + logger

--- controller/crudControllers.js
+++ controller/crudControllers.js
@@ -9,7 +9,6 @@ const {
   deleteModel,
   updateModel,
 } = require("../model/models");
-// var validator = require("email-validator");
 dotenv.config();
 
 /*************************Add data************** */
@@ -17,58 +16,18 @@ dotenv.config();
 const addData = async (req, res) => {
   try {
     const [rows] = await addModel(req.body);
-    // if (rows.affectedRows === 1) {
     logger.info(rows);
     res.json({
-      status: "success",
+      status: "success (try)",
       message: "Record Inserted successfully",
     });
-    // }
   } catch (err) {
     logger.error(err);
     res.status(400).json({
-      status: "fail",
-      message: err.message,
+      status: "fail (catch)",
+      message: err.message + " Inserted data is not correct",
     });
   }
-
-  // getModel(req, res);
-  // const { email, name, cnic, designation, contact } = req.body;
-  // /****************************Plain Password********************* */
-  // const plain_password = req.body.protected_password;
-  // /****************************Protected Password****************** */
-  // let protected_password = req.body.protected_password;
-  // const salt = await bcrypt.genSalt(10);
-  // protected_password = await bcrypt.hash(protected_password, salt);
-  // const data = [
-  //   email,
-  //   plain_password,
-  //   protected_password,
-  //   name,
-  //   cnic,
-  //   designation,
-  //   contact,
-  // ];
-  // // if (validator.validate(email))
-  // const insert_query =
-  //   "INSERT into crud_table (email,plain_password,protected_password,name,cnic,designation,contact) values(?,?,?,?,?,?,?)";
-  // try {
-  //   await connection.query(
-  //     insert_query, //2. saving in database
-  //     data,
-  //     function (err, result) {
-  //       if (err) {
-  //         logger.info(err);
-  //         res.status(400);
-  //       } else {
-  //         logger.info(res.json({ result }));
-  //         logger.info("data inserted successfully");
-  //       }
-  //     }
-  //   );
-  // } catch (err) {
-  //   res.send(err);
-  // }
 };
 
 /************************Read Data***************** */
@@ -76,14 +35,8 @@ const addData = async (req, res) => {
 const viewData = async (req, res) => {
   try {
     const [rows] = await getModel(req.body);
-    // if (rows.affectedRows === 1) {
-    logger.info(rows);
-    logger.info(res.json({ rows })); // show in swagger
-    res.json({
-      status: "success",
-      message: "Record Viewed successfully",
-    });
-    // }
+    res.json({ rows }); // show in swagger
+    logger.info("Data view successfuly");
   } catch (err) {
     logger.error(err);
     res.status(400).json({
@@ -92,37 +45,16 @@ const viewData = async (req, res) => {
     });
   }
 };
-// const viewData = async (req, res) => {
-//   const viewAllData = "select * from crud_table";
-//   try {
-//     await connection.query(viewAllData, (err, rows) => {
-//       if (err) {
-//         res.send(err);
-//       } else {
-//         logger.info(res.json({ rows }));
-//         logger.info("Data read success");
-//       }
-//     });
-//   } catch (err) {
-//     res.send(err);
-//   }
-// };
 
 /*******************Read by Id************************/
 
 const viewDataById = async (req, res) => {
   try {
     const [rows] = await getModelbyId(req.params.id);
-    // if (rows.affectedRows === 1) {
     logger.info("data viewed by ID succesfully");
-    // logger.info(rows);
-    logger.info(res.json({ rows })); // show in swagger
-
-    // res.json({
-    //   status: "success",
-    //   message: "Record Viewed successfully",
-    // });
-    // }
+    logger.info(rows);
+    res.json({ rows }); // show in swagger
+    logger.info("data viewed by id");
   } catch (err) {
     logger.error(err);
     res.status(400).json({
@@ -131,39 +63,16 @@ const viewDataById = async (req, res) => {
     });
   }
 };
-
-// const viewDataById = async (req, res) => {
-//   const id = req.params.id;
-//   const viewAllDataById = "select * from crud_table WHERE id=?";
-//   try {
-//     await connection.query(viewAllDataById, [id], (err, rows) => {
-//       if (err) {
-//         res.send(err);
-//       } else {
-//         logger.info(res.json({ rows }));
-//         logger.info("data viewed by ID succesfully");
-//       }
-//     });
-//   } catch (err) {
-//     res.send(err);
-//   }
-// };
 
 /*******************Delete Data******************** */
 
 const deleteData = async (req, res) => {
   try {
     const [rows] = await deleteModel(req.params.id);
-    // if (rows.affectedRows === 1) {
     logger.info("data delete by ID succesfully");
-    // logger.info(rows);
-    logger.info(res.json({ rows })); // show in swagger
-
-    // res.json({
-    //   status: "success",
-    //   message: "Record Viewed successfully",
-    // });
-    // }
+    logger.info(rows);
+    res.json({ rows }); // show in swagger
+    logger.info("data deleted successfuly");
   } catch (err) {
     logger.error(err);
     res.status(400).json({
@@ -172,76 +81,22 @@ const deleteData = async (req, res) => {
     });
   }
 };
-
-// const deleteData = async (req, res) => {
-//   const id = req.params.id;
-//   const deleteData = "DELETE FROM crud_table WHERE id=?";
-//   try {
-//     await connection.query(deleteData, [id], (err, rows) => {
-//       if (err) {
-//         res.send(err);
-//       } else {
-//         logger.info(res.json({ rows }));
-//         logger.info("data deleted sucessfully");
-//       }
-//     });
-//   } catch (err) {
-//     res.send(err);
-//   }
-// };
 
 /********************Update Data************************** */
 
-//issue
 const updateData = async (req, res) => {
   try {
     const [rows] = await updateModel(req);
-    // if (rows.affectedRows === 1) {
-    logger.info(rows);
-    res.json({
-      status: "success",
-      message: "Record updated successfully",
-    });
-    // }
+    logger.info(res.json({ rows }));
+    logger.info("Data is updated successfully");
   } catch (err) {
     logger.error(err);
     res.status(400).json({
-      status: "fail",
-      message: err.message,
+      status: "fail (catch)",
+      message: err.message + " Inserted data is not correct",
     });
   }
 };
-
-// const updateData = async (req, res) => {
-//   const user = req.body;
-//   const { id } = req.params;
-//   const { email, name, cnic, designation, contact } = req.body;
-
-//   /********************Plain updated Password************ */
-//   const plain_password = req.body.protected_password;
-
-//   /*********************Protected Updated Password********* */
-//   let protected_password = req.body.protected_password;
-//   const salt = await bcrypt.genSalt(10);
-//   protected_password = await bcrypt.hash(protected_password, salt);
-
-//   const query = `email = '${email}',plain_password = '${plain_password}',protected_password = '${protected_password}',
-//   name = '${name}',cnic = '${cnic}',designation = '${designation}',contact = '${contact}'`;
-//   const updateData = `UPDATE crud_table SET ${query} where id=` + id;
-
-//   try {
-//     await connection.query(updateData, [user], (err, rows) => {
-//       if (err) {
-//         logger.info(res.send(err));
-//       } else {
-//         logger.info(res.send(rows));
-//         logger.info("data updated successfully");
-//       }
-//     });
-//   } catch (err) {
-//     res.send(err);
-//   }
-// };
 
 module.exports = {
   viewData,
--- model/models.js
+++ model/models.js
@@ -1,15 +1,22 @@
 const bcrypt = require("bcrypt");
 const connection = require("../config/db");
+const validator = require("email-validator");
+const logger = require("../logger");
+const passwordValidator = require("password-validator");
+const schema = new passwordValidator();
+schema
+  .is()
+  .min(8) // Minimum length 8
+  .has()
+  .uppercase() // Must have uppercase letters
+  .has()
+  .lowercase() // Must have lowercase letters
+  .has()
+  .digits(); // Must have digits
 
 exports.addModel = async values => {
   const { email, name, cnic, designation, contact, plain_password } = values;
 
-  /****************************Plain Password********************* */
-  //   const plain_password = values.protected_password;
-
-  /****************************Protected Password****************** */
-  //   let protected_password = values.protected_password;
-  // console.log(plain_password);
   values.protected_password = await bcrypt.hash(plain_password, 13);
 
   const data = [
@@ -21,14 +28,31 @@ exports.addModel = async values => {
     designation,
     contact,
   ];
-  // if (validator.validate(email))
+
   const insert_query =
     "INSERT into crud_table (email,plain_password,protected_password,name,cnic,designation,contact) values(?,?,?,?,?,?,?)";
 
-  return connection.promise().query(
-    insert_query, //2. saving in database
-    data
-  );
+  if (validator.validate(email) && schema.validate(plain_password)) {
+    return connection.promise().query(
+      insert_query, //2. saving in database
+      data
+    );
+  } else {
+    logger.info(
+      "Email is invalid/Passwword: password must have min 8 character (Uppercase,Lowercase and digits only) "
+    );
+  }
+
+  if (validator.validate(email) && schema.validate(plain_password)) {
+    return connection.promise().query(
+      insert_query, //2. saving in database
+      data
+    );
+  } else {
+    logger.info(
+      "Email is invalid/Passwword: password must have min 8 character (Uppercase,Lowercase and digits only) "
+    );
+  }
 };
 
 exports.getModel = async () => {
@@ -52,16 +76,20 @@ exports.updateModel = async req => {
   const user = req.body;
   const { id } = req.params;
   const { email, name, cnic, designation, contact, plain_password } = req.body;
-
-  /*********************Protected Updated Password********* */
   req.protected_password = await bcrypt.hash(plain_password, 13);
 
   const query = `email = '${email}',plain_password = '${plain_password}',protected_password = '${req.protected_password}',
   name = '${name}',cnic = '${cnic}',designation = '${designation}',contact = '${contact}'`;
   const updateData = `UPDATE crud_table SET ${query} where id=` + id;
 
-  return connection.promise().query(
-    updateData, //2. saving in database
-    [user]
-  );
+  if (validator.validate(email) && schema.validate(plain_password)) {
+    return connection.promise().query(
+      updateData, //2. saving in database
+      [user]
+    );
+  } else {
+    logger.info(
+      "Email is invalid/Passwword: password must have min 8 character (Uppercase,Lowercase and digits only) "
+    );
+  }
 };
