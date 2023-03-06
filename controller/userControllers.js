c173a8b54c940533d6368703a4f482af289989a5
asif-mehmood@hotmail.com <asif-mehmood@hotmail.com>
2023-03-06 13:09:33 +0500

    full login and signup

--- controller/userControllers.js
+++ controller/userControllers.js
@@ -1,5 +1,5 @@
 const logger = require("../logger");
-const { signupModel } = require("../model/userModels");
+const { signupModel, loginModel } = require("../model/userModels");
 const sendMail = require("./sendMailController");
 const connection = require("../config/db");
 const bcrypt = require("bcrypt");
@@ -30,46 +30,7 @@ const login = async (req, res) => {
   const secretKey = process.env.secretKey;
   try {
     const { email, password } = req.body;
-    if (!email || !password) {
-      return res.status(400).json({
-        message: "Please Provide an email and password",
-      });
-    }
-    connection.query(
-      "SELECT * FROM users WHERE email = ?",
-      [email],
-      async (err, results) => {
-        logger.info(results);
-        if (
-          !results ||
-          !(await bcrypt.compare(password, results[0].password))
-        ) {
-          res.status(401).json({
-            message: "Email or Password is incorrect",
-          });
-        } else {
-          const id = results[0].id;
-
-          const token = jwt.sign({ id, email }, secretKey, {
-            expiresIn: process.env.JWT_EXPIRES,
-          });
-
-          logger.info("the token has been generated " + token);
-
-          const cookieOptions = {
-            expires: new Date(
-              Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
-            ), //converted into milli sec
-            httpOnly: true,
-          };
-          res.cookie("userRegistered", token, cookieOptions);
-          res.status(200).json({
-            status: "success",
-            sucess: "User has been logged in",
-          });
-        }
-      }
-    );
+    await loginModel(email, password, secretKey, res);
   } catch (err) {
     logger.info(err);
   }
--- model/userModels.js
+++ model/userModels.js
@@ -3,6 +3,7 @@ const connection = require("../config/db");
 const logger = require("../logger");
 const password_schema = require("../middleware/passwordValidation");
 const email_validator = require("../middleware/emailValidation");
+const jwt = require("jsonwebtoken");
 
 exports.signupModel = async (registration_data, res) => {
   const { email, confirm_password, password, cnic } = registration_data;
@@ -60,5 +61,74 @@ exports.signupModel = async (registration_data, res) => {
       message:
         error.message + " Inserted data is not correct or already exists",
     });
+  }
+};
+
+exports.loginModel = async (email, password, secretKey, res) => {
+  try {
+    if (email_validator.validate(email)) {
+      if (password_schema.validate(password)) {
+        if (!email || !password) {
+          return res.status(400).json({
+            message: "Please Provide an email and password",
+          });
+        }
+        await connection.query(
+          "SELECT * FROM users WHERE email = ?",
+          [email],
+          async (err, results) => {
+            logger.info(results);
+            if (
+              !results ||
+              !(await bcrypt.compare(password, results[0].password))
+            ) {
+              res.status(401).json({
+                message: "Email or Password is incorrect",
+              });
+            } else {
+              const id = results[0].id;
+
+              const token = jwt.sign({ id, email }, secretKey, {
+                expiresIn: process.env.JWT_EXPIRES,
+              });
+
+              logger.info("the token has been generated " + token);
+
+              const cookieOptions = {
+                expires: new Date(
+                  Date.now() +
+                    process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+                ), //converted into milli sec
+                httpOnly: true,
+              };
+              res.cookie("userRegistered", token, cookieOptions);
+              res.status(200).json({
+                status: "success",
+                sucess: "User has been logged in",
+              });
+            }
+          }
+        );
+      } else {
+        logger.info(
+          "Password must have min 8 character and must contain (Uppercase,Lowercase and digits only)  "
+        );
+        return res.status(403).json({
+          status: "incorrect password format",
+          message:
+            "Password must have min 8 character and must contain (Uppercase,Lowercase and digits only) ",
+        });
+      }
+    } else {
+      logger.info(
+        "Incorrect email format. Email format should be:asif@email.com "
+      );
+      return res.status(403).json({
+        status: "incorrect email format",
+        message: "email format should be: asif@email.com  ",
+      });
+    }
+  } catch (err) {
+    logger.info(err);
   }
 };
